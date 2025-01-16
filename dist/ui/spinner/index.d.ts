b046ac15b011e5c2755dd084b38b1aedb7342423
Zeynalli Zeynal <zzeynalli446@gmail.com>
2025-01-17 01:41:01 +0400

    feat(ui): add spinner component

---
+++ dist/ui/spinner/index.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+declare const Spinner: ({ size }: {
+    size?: number;
+}) => React.JSX.Element;
+export default Spinner;
+//# sourceMappingURL=index.d.ts.map
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -3,3 +3,5 @@ export type * from "./tabs/tabs.types";
 
 export * from "./collapse";
 export type * from "./collapse/collapse.types";
+
+export * from "./spinner";
