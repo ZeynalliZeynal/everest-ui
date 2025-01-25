9f5a23fb742350e629f06799be9def9b6098b9b4
ZeynalliZeynal <zzeynalli446@gmail.com>
2025-01-25 15:52:02 +0400

    fix: change target to esnext in tsup config

--- packages/everest-ui/tsup.config.ts
+++ packages/everest-ui/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  target: "node18",
+  target: "esnext",
   sourcemap: true,
   splitting: false,
   dts: true,
