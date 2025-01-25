3c7b1aa6eb1c3b16531b5351fc9c0f53e153732a
ZeynalliZeynal <zzeynalli446@gmail.com>
2025-01-25 16:04:41 +0400

    add execa as a dev dependency

--- packages/everest-ui/tsup.config.ts
+++ packages/everest-ui/tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  target: "esnext",
+  format: ["esm"],
   sourcemap: true,
   splitting: false,
   dts: true,
