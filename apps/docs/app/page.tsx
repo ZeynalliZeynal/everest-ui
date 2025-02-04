df032b46518a43008d311f5085fe706f8e72c96c
Zeynalli Zeynal <zzeynalli446@gmail.com>
2025-02-04 13:33:06 +0400

    setup tailwindcss in docs app

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -11,27 +11,35 @@ import {
   DropdownMenuSubContent,
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
-} from "@everest-ui/geist-dropdown-menu";
+} from "@everest-ui/react-dropdown-menu";
 
 export default function Page() {
   return (
     <div className={styles.page}>
       <Button>Test</Button>
       <DropdownMenu>
-        <DropdownMenuTrigger>popper</DropdownMenuTrigger>
-        <DropdownMenuContent>
+        <DropdownMenuTrigger asChild>
+          <Button>npm i tailwindcss-animate</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='z-50 min-w-[8rem] animate-in fade-in zoom-in overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 w-56'>
           <DropdownMenuGroup>
             <DropdownMenuLabel>Label</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuSub>
               <DropdownMenuSubTrigger>lakama</DropdownMenuSubTrigger>
               <DropdownMenuSubContent>
-                <DropdownMenuItem>rasdas</DropdownMenuItem>
+                <DropdownMenuItem className='relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0'>
+                  rasdas
+                </DropdownMenuItem>
               </DropdownMenuSubContent>
             </DropdownMenuSub>
-            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem className='relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0'>
+              Profile
+            </DropdownMenuItem>
             <DropdownMenuItem disabled>Disabled</DropdownMenuItem>
-            <DropdownMenuItem>Logout</DropdownMenuItem>
+            <DropdownMenuItem className='relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0'>
+              Logout
+            </DropdownMenuItem>
           </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
